348dcb801e107f24d7ecfc4668204430cbdb5f87
Atang Sutisna <atang.sutisna.87@gmail.com>
2019-07-14 19:49:15 +0700

    fix select nominal credits

--- application/views/transaction/index.php
+++ application/views/transaction/index.php
@@ -1,10 +1,10 @@
 <section class="content-header">
     <h1>
-        Transaction
+        Transaksi Pulsa
     </h1>
     <ol class="breadcrumb">
         <li><a href="<?= site_url('dashboard') ?>"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active">Transaction</li>
+        <li class="active">Transaksi Pulsa</li>
     </ol>
 </section>
 <div class="content">
@@ -15,10 +15,10 @@
                 <div class="box-body">
                     <form class="form-horizontal" id="form_transaction">
                         <div class="form-group">
-                            <label class="col-3 col-sm-3 col-lg-3 control-label">Phone Number</label>
+                            <label class="col-3 col-sm-3 col-lg-3 control-label">Nomor Hp</label>
                             <div class="col-4 col-sm-4 col-lg-4">
                                 <div class="input-group">
-                                    <input type="text" name="phone_number" class="form-control" v-model="phone_number"/>
+                                    <input type="text" name="phone_number" class="form-control" v-model="phone_number" maxlength="12"/>
                                     <div class="input-group-addon" v-if="operator_name !== null">{{operator_name}}</div>
                                 </div>
                             </div>
@@ -26,24 +26,15 @@
                         <div class="form-group">
                             <label class="col-3 col-sm-3 col-lg-3 control-label">Nominal</label>
                             <div class="col-8 col-sm-8 col-lg-8">
-                                <label>
-                                    <input type="radio" name="credit_nominal" selected/> Rp. 5000
-                                </label>
-                                <label>
-                                    <input type="radio" name="credit_nominal"/> Rp. 10000
-                                </label>
-                                <label>                                    
-                                    <input type="radio" name="credit_nominal"/> Rp. 15000
-                                </label>
-                                <label>                                    
-                                    <input type="radio" name="credit_nominal"/> Rp. 25000
+                                <label v-for="nominal in nominals" style="padding-left: 5px;">
+                                    <input type="radio" name="credit_nominal" v-bind:value="nominal" v-model="credit_nominal"/>{{nominal.nominal}}
                                 </label>
                             </div>
                         </div>
                         <div class="form-group">
-                            <label class="col-3 col-sm-3 col-lg-3 control-label">Price</label>
+                            <label class="col-3 col-sm-3 col-lg-3 control-label">Harga</label>
                             <div class="col-6 col-sm-6 col-lg-6">
-                                <p class="form-control-static">Rp. 6000</p>
+                                <p class="form-control-static">{{price}}</p>
                             </div>
                         </div>
                         <div class="form-group">
@@ -55,12 +46,11 @@
                     <div class="table-responsive">
                         <table class="table table-striped" id="table_transaction">
                             <thead>
-                                <th>Date</th>
-                                <th>Operator Name</th>
-                                <th>Phone number</th>
+                                <th>Tanggal</th>
+                                <th>Operator</th>
+                                <th>Nomor Hp</th>
                                 <th>Nominal</th>
-                                <th>Price</th>
-                                <th>Status</th>
+                                <th>Harga</th>
                             </thead>
                             <tbody>
                             </tbody>
--- assets/js/transaction/index.js
+++ assets/js/transaction/index.js
@@ -34,33 +34,53 @@ $('document').ready(function(){
                     return accounting.formatMoney(data);
                 }
             },
-            { 
-                "orderable": false,
-                "data": "status",
-            },
         ]
     }); 
 
 var form_transaction = new Vue({
     el: '#form_transaction',
     data: {
+        operator_id: null,
+        operator_name: null,
         phone_number: null,
-        operator_name: null
+        credit_nominal_id: null,
+        credit_nominal: null,
+        price: 0,
+        nominals: []
     },
     watch: {
+        credit_nominal: function(val) {
+            app = this;
+            app.credit_nominal_id = val.id;
+            app.price = val.price;
+            console.info('credit nominal id: '+ app.credit_nominal_id);
+        },
         phone_number: function(val) {
             app = this;
-            if (val !== undefined && val !== '' && val.length === 4) {
+            if (val !== undefined && val !== '' && val.length == 4) {
                 $.get(app_config.api_uri + "/operator_prefix?prefix="+ val, function(data){
                     if (data.operator != null) {
+                        app.operator_id = data.operator.id;
                         app.operator_name = data.operator.name;
+                        app.nominals = [];
+                        $.each(data.operator.nominals, function(index, value){
+                            app.nominals.push({
+                                'id': value.id,
+                                'nominal': value.nominal,
+                                'price': accounting.formatMoney(value.original_price),
+                            });
+                        });
                     } else {
                         console.info("unknown operator");
+                        app.operator_id = null;
                         app.operator_name = null;
+                        app.nominals = [];
                     }
                 });
-            } else {
+            } else if (val !== undefined && val !== '' && val.length < 4) {
+                app.operator_id = null;
                 app.operator_name = null;
+                app.nominals = [];
             }
         }
     }
