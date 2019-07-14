42506dd778ea15437f3cfc85ca8aaec1b59381dc
Atang Sutisna <atang.sutisna.87@gmail.com>
2019-07-14 10:12:09 +0700

    fix form transaction

--- application/views/transaction/index.php
+++ application/views/transaction/index.php
@@ -13,6 +13,34 @@
             <?= show_bootstrap_alert() ?>
             <div class="box">
                 <div class="box-body">
+                    <form class="form-horizontal" id="form_filter">
+                        <div class="form-group">
+                            <label class="col-3 col-sm-3 col-lg-3 control-label">Phone Number</label>
+                            <div class="col-4 col-sm-4 col-lg-4">
+                                <div class="input-group">
+                                    <input type="text" name="phone_number" class="form-control"/>
+                                    <div class="input-group-addon">Axis</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-3 col-sm-3 col-lg-3 control-label">Nominal</label>
+                            <div class="col-3 col-sm-3 col-lg-3">
+
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-3 col-sm-3 col-lg-3 control-label">Price</label>
+                            <div class="col-6 col-sm-6 col-lg-6">
+
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-offset-3 col-4 col-sm-4 col-lg-4">
+                                <button type="submit" class="btn btn-success" id="btn-reset">Process</button>
+                            </div>
+                        </div>
+                    </form>
                     <div class="table-responsive">
                         <table class="table table-striped" id="table_transaction">
                             <thead>
