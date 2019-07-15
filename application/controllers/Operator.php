b4bd659b789a71ee367d3be4e173132da010a22a
Atang Sutisna <atang.sutisna.87@gmail.com>
2019-07-15 07:18:10 +0700

    fix insert new operator

--- application/controllers/Operator.php
+++ application/controllers/Operator.php
@@ -13,4 +13,37 @@ class Operator extends Admin_Controller
         );
         $this->load->template(self::DIR_VIEW. '/index', $params);
     }
+
+    public function reg_form()
+    {
+        $params = [
+            'form_action' => site_url('operator/do_reg')
+        ];
+        $this->load->template(self::DIR_VIEW. '/form', $params);
+    }
+
+    public function do_reg()
+    {
+        $this->load->library('form_validation');
+        $this->form_validation->set_error_delimiters('<p class="text-danger">', '</p>');
+
+        $this->form_validation->set_rules('name', 'Nama', 'required');
+        $this->form_validation->set_rules('prefix', 'Prefix', 'required');
+        if ($this->form_validation->run() == FALSE) {
+            $this->reg_form();
+        } else {
+            $new_operator = [
+                'name' => $this->input->post('name'),
+                'prefixs' => $this->input->post('prefix'),
+                'creation_time' => date('Y-m-d H:i:s'),
+                'modification_time' => date('Y-m-d H:i:s')
+            ];
+
+            $this->load->model('Operator_model', 'operator');
+            $this->operator->insert($new_operator);            
+            $this->session->set_flashdata('info', '1 operator telah berhasil ditambahkan');
+            redirect('operator');
+        }
+    }
+
 }
---
+++ application/views/operator/form.php
@@ -0,0 +1,47 @@
+<section class="content-header">
+    <h1>
+        Operator
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="<?= site_url('dashboard') ?>"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Tambah Operator</li>
+    </ol>
+</section>
+<div class="content">
+    <div class="row">
+        <div class="col-lg-12 col-sm-12 col-12">
+            <?= show_bootstrap_alert() ?>
+            <div class="box">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Operator</h3>
+                </div>
+                <div class="box-body">
+                    <form role="form" method="post" action="<?= $form_action ?>" class="form-horizontal"> 
+                        <div class="form-group">
+                            <label class="col-lg-3 col-sm-3 control-label">Nama</label>
+                            <div class="col-lg-4 col-sm-4">
+                                <input type="text" name="name" 
+                                    class="form-control" value="<?= set_value('name', isset($operator) ? $operator->name : '') ?>"/>
+                                <?= form_error('name') ?>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-lg-3 col-sm-3 control-label">Prefix</label>
+                            <div class="col-lg-5 col-sm-5">
+                                <input type="text" name="prefix" 
+                                    class="form-control" value="<?= set_value('name', isset($operator) ? $operator->prefix : '') ?>"/>
+                                <?= form_error('prefix') ?>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-lg-offset-3 col-sm-offset-3" style="margin-left: 26.5%;">
+                                <button type="submit" class="btn btn-primary">Simpan</button>
+                                <?= anchor('operator', 'Kembali', ['class' => 'btn btn-default']) ?>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
