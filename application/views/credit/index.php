99a23c6238b4715aafad3419c579312fde22ccf0
Atang Sutisna <atang.sutisna.87@gmail.com>
2019-07-15 08:08:41 +0700

    convert to indonesian

--- application/views/credit/index.php
+++ application/views/credit/index.php
@@ -1,10 +1,10 @@
 <section class="content-header">
     <h1>
-        Credit
+        Pulsa
     </h1>
     <ol class="breadcrumb">
-        <li><a href="<?= site_url('dashboard') ?>"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active">Credit</li>
+        <li><a href="<?= site_url('home') ?>"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Pulsa</li>
     </ol>
 </section>
 <div class="content">
@@ -13,15 +13,15 @@
             <?= show_bootstrap_alert() ?>
             <div class="box">
                 <div class="box-body">
-                    <p><a href="<?= site_url('credit/reg_form') ?>" class="btn btn-primary">Credit Baru</a></p>
+                    <p><a href="<?= site_url('credit/reg_form') ?>" class="btn btn-primary">Nominal Baru</a></p>
                     <div class="table-responsive">
                         <table class="table table-striped" id="table_credit">
                             <thead>
-                                <th>Last update</th>
-                                <th>Operator Name</th>
+                                <th>Tanggal</th>
+                                <th>Operator</th>
                                 <th>Nominal</th>
-                                <th>Original Price</th>
-                                <th>Price</th>
+                                <th>Harga Provider</th>
+                                <th>Harga Jual</th>
                                 <th>#</th>
                             </thead>
                             <tbody>
