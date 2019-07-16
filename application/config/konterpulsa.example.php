bd0b1c0d71e9e2a24d650af5d052c80af389e2c1
Atang Sutisna <atang.sutisna.87@gmail.com>
2019-07-16 22:13:28 +0700

    add guzzle for rest client

---
+++ application/config/konterpulsa.example.php
@@ -0,0 +1,5 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['mobilepulsa']['username'] = '083821230266';
+$config['mobilepulsa']['apikey']  = '7825d2de6aba30f1';
