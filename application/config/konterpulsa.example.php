fbfe32887dd69b2ebc8a81b45f06cbb864643023
Atang Sutisna <atang@bippo.co.id>
2019-08-23 02:55:10 -0400

    change username and apikey

--- application/config/konterpulsa.example.php
+++ application/config/konterpulsa.example.php
@@ -1,5 +1,5 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['mobilepulsa']['username'] = '083821230266';
-$config['mobilepulsa']['apikey']  = '7825d2de6aba30f1';
+$config['mobilepulsa']['username'] = 'your-username';
+$config['mobilepulsa']['apikey']  = 'your-apikey';
